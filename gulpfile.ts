55aed085627869c6e7e79db640aec0e78b0e2b04
matthew harwood <matthhar12@gmail.com>
2016-05-22 01:44:09 -0700

    - init build.

---
+++ gulpfile.ts
@@ -0,0 +1,76 @@
+/**
+ * Created by matthewharwood on 5/21/2016.
+ */
+import {Gulpclass, Task, SequenceTask} from "gulpclass/Decorators";
+
+
+let gulp = require('gulp');
+let del = require('del');
+let ts = require('gulp-typescript');
+let browserSync = require('browser-sync').create();
+
+@Gulpclass()
+export class Gulpfile {
+  private dist: string;
+  private src: string;
+
+  constructor(){
+    this.dist = './dist/';
+    this.src = './src/'
+  }
+
+  @Task()
+  clean(cb: Function) {
+    return del([`${this.dist}**`], cb);
+  }
+
+  @Task('sync')
+  sync() {
+    browserSync.init(null, {
+      logLevel: 'debug',
+      proxy: 'http://localhost:1337',
+      files: [`${this.dist}/client/**/*.{html,css,js}`],
+      browser: 'google chrome',
+      reloadDelay: 1000,
+      port: 7002
+    });
+  }
+
+  @Task('copy-source-files') // you can specify custom task name if you need
+  copySourceFiles() {
+    return gulp.src([`${this.src}**/*.{png,gif,jpg,html}`])
+        .pipe(gulp.dest(`${this.dist}`));
+  }
+
+  @Task('file-source-watcher')
+  watcher() {
+
+    gulp.watch(`${this.src}/**/*.{png,gif,jpg,html}`, ['run']);
+    //gulp.watch(`${this.src}/**/*.scss`, ['styles']);
+
+  }
+
+  @Task('tsd')
+  compileServer() {
+    return gulp.src(`${this.src}**/*.ts`)
+        .pipe(ts({
+          noImplicitAny: true,
+          'outDir': "build/es5",
+          'target': "es5",
+          'module': "commonjs",
+          'moduleResolution': "node",
+          'emitDecoratorMetadata': true,
+          'experimentalDecorators': true,
+          'sourceMap': true,
+          'declaration': true,
+          outDir: this.dist
+        }))
+        .pipe(gulp.dest(this.dist));
+  }
+
+  @SequenceTask('run') // this special annotation using "run-sequence" module to run returned tasks in sequence
+  build() {
+    return ['copy-source-files', 'tsd'];
+  }
+
+}
